cc6cd3770b7e8075c4b7804d7cd94a34f0de302d
Gab <52910644+gabhm@users.noreply.github.com>
2021-03-10 22:25:21 -0300

    Update getLink.js

--- scripts/getLink.js
+++ scripts/getLink.js
@@ -1,10 +1,22 @@
 const fetch = require('node-fetch')
 const Bluebird = require('bluebird')
+const fs = require('fs')
 fetch.Promise = Bluebird
 
 function get_link(){
     fetch('http://127.0.0.1:4040/api/tunnels')
     .then(data => data.json())
-    .then(body => console.log(`Pishing Link: ${body['tunnels'][0]['public_url']}`))
+    .then(body => {
+
+      fs.writeFile(`link.json`, `
+{
+    "link":"${body['tunnels'][0]['public_url']}"
+}`, (err, data)=>{
+
+
+        })
+
+    })
 }
 module.exports = get_link
+
