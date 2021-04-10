837e7405613bae2b0b427c7cfceae0ca48944cd6
Gab <52910644+gabhm@users.noreply.github.com>
2021-04-10 03:24:48 -0300

    Delete start.sh

--- src/start.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash
-./ngrok http 1337 > /dev/null 2>&1 & sleep 10
-curl -s -N http://127.0.0.1:4040/api/tunnels | grep -o "https://[0-9a-z]*\.ngrok.io" > link.txt 
-node .
-
-
-pkill -f -2 ngrok > /dev/null 2>&1
-killall -2 ngrok > /dev/null 2>&1
-printf "break ngrok !!\n"
